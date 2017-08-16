1819e6bb1b3303bba4cdcf490bf9ae33f7ca9b3f
sunjinshuai <s_mike@163.com>
2017-08-16 19:08:31 +0800

    +load和+initialize的区别

--- MYUtils/MYUtils/Resource/main.m
+++ MYUtils/MYUtils/Resource/main.m
@@ -8,9 +8,16 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Parent.h"
+#import "Child.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+//    @autoreleasepool {
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
+    
+    Child *child = [[Child alloc] init];
+    
+    NSLog(@"%@",child);
+    
 }
