57be82f93e5fc8e8d5b6c734b3de84978c70971d
sunjinshuai <s_mike@163.com>
2017-08-27 17:06:14 +0800

    NSString扩展

--- MYUtils/MYUtils/Category/Foundation/NSString+Extension.m
+++ MYUtils/MYUtils/Category/Foundation/NSString+Extension.m
@@ -24,4 +24,186 @@
     return @"";
 }
 
+- (NSString *)stringByStrippingHTML {
+    return [self stringByReplacingOccurrencesOfString:@"<[^>]+>" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, self.length)];
+}
+
+- (NSString *)stringByRemovingScriptsAndStrippingHTML {
+    NSMutableString *mString = [self mutableCopy];
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"<script[^>]*>[\\w\\W]*</script>" options:NSRegularExpressionCaseInsensitive error:&error];
+    NSArray *matches = [regex matchesInString:mString options:NSMatchingReportProgress range:NSMakeRange(0, [mString length])];
+    for (NSTextCheckingResult *match in [matches reverseObjectEnumerator]) {
+        [mString replaceCharactersInRange:match.range withString:@""];
+    }
+    return [mString stringByStrippingHTML];
+}
+
+- (NSString *)trimmingWhitespace {
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+}
+
+- (BOOL)isValidateByRegex:(NSString *)regex {
+    NSPredicate *pre = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [pre evaluateWithObject:self];
+}
+
+// 手机号分服务商
+- (BOOL)isMobileNumberClassification {
+    /**
+     * 手机号码
+     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188,1705,183
+     * 联通：130,131,132,152,155,156,185,186,1709
+     * 电信：133,1349,153,180,189,1700
+     */
+    //    NSString * MOBILE = @"^1((3//d|5[0-35-9]|8[025-9])//d|70[059])\\d{7}$";//总况
+    
+    /**
+     10         * 中国移动：China Mobile
+     11         * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188，1705
+     12         */
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[0-2,7-9]|8[2378])\\d|705)\\d{7}$";
+    /**
+     15         * 中国联通：China Unicom
+     16         * 130,131,132,152,155,156,185,186,1709
+     17         */
+    NSString * CU = @"^1((3[0-2]|5[256]|8[56])\\d|709)\\d{7}$";
+    /**
+     20         * 中国电信：China Telecom
+     21         * 133,1349,153,180,189,1700
+     22         */
+    NSString * CT = @"^1((33|53|8[09])\\d|349|700)\\d{7}$";
+    
+    
+    /**
+     25         * 大陆地区固话及小灵通
+     26         * 区号：010,020,021,022,023,024,025,027,028,029
+     27         * 号码：七位或八位
+     28         */
+    NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+    
+    
+    //    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    
+    if (([self isValidateByRegex:CM])
+        || ([self isValidateByRegex:CU])
+        || ([self isValidateByRegex:CT])
+        || ([self isValidateByRegex:PHS])) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+// 手机号有效性
+- (BOOL)isMobileNumber {
+    NSString *mobileRegex = @"^(0|86|17951)?(13[0-9]|15[012356789]|17[0678]|18[0-9]|14[57])[0-9]{8}$";
+    BOOL ret1 = [self isValidateByRegex:mobileRegex];
+    return ret1;
+}
+
+// 邮箱
+- (BOOL)isEmailAddress {
+    NSString *emailRegex = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$";
+    return [self isValidateByRegex:emailRegex];
+}
+
+// 身份证号
+- (BOOL)simpleVerifyIdentityCardNum {
+    NSString *regex2 = @"^(\\d{14}|\\d{17})(\\d|[xX])$";
+    return [self isValidateByRegex:regex2];
+}
+
+// 车牌
+- (BOOL)isCarNumber {
+    //车牌号:湘K-DE829 香港车牌号码:粤Z-J499港
+    NSString *carRegex = @"^[\u4e00-\u9fff]{1}[a-zA-Z]{1}[-][a-zA-Z_0-9]{4}[a-zA-Z_0-9_\u4e00-\u9fff]$";//其中\u4e00-\u9fa5表示unicode编码中汉字已编码部分，\u9fa5-\u9fff是保留部分，将来可能会添加
+    return [self isValidateByRegex:carRegex];
+}
+
+- (BOOL)isMacAddress {
+    NSString * macAddRegex = @"([A-Fa-f\\d]{2}:){5}[A-Fa-f\\d]{2}";
+    return  [self isValidateByRegex:macAddRegex];
+}
+
+- (BOOL)isValidUrl {
+    NSString *regex = @"^((http)|(https))+:[^\\s]+\\.[^\\s]*$";
+    return [self isValidateByRegex:regex];
+}
+
+- (BOOL)isValidChinese {
+    NSString *chineseRegex = @"^[\u4e00-\u9fa5]+$";
+    return [self isValidateByRegex:chineseRegex];
+}
+
+- (BOOL)isValidPostalcode {
+    NSString *postalRegex = @"^[0-8]\\d{5}(?!\\d)$";
+    return [self isValidateByRegex:postalRegex];
+}
+
+- (BOOL)isValidTaxNo {
+    NSString *taxNoRegex = @"[0-9]\\d{13}([0-9]|X)$";
+    return [self isValidateByRegex:taxNoRegex];
+}
+
+- (BOOL)isContainChinese {
+    NSUInteger length = [self length];
+    for (NSUInteger i = 0; i < length; i++) {
+        NSRange range = NSMakeRange(i, 1);
+        NSString *subString = [self substringWithRange:range];
+        const char *cString = [subString UTF8String];
+        if (strlen(cString) == 3) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (NSString *)trimmingWhitespaceAndNewlines {
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+- (NSDictionary *)dictionaryValue {
+    NSError *errorJson;
+    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:[self dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&errorJson];
+    if (errorJson != nil) {
+#ifdef DEBUG
+        NSLog(@"fail to get dictioanry from JSON: %@, error: %@", self, errorJson);
+#endif
+    }
+    return jsonDict;
+}
+
+- (CGSize)sizeForFont:(UIFont *)font size:(CGSize)size mode:(NSLineBreakMode)lineBreakMode {
+    CGSize result;
+    if (!font) font = [UIFont systemFontOfSize:12];
+    if ([self respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)]) {
+        NSMutableDictionary *attr = [NSMutableDictionary new];
+        attr[NSFontAttributeName] = font;
+        if (lineBreakMode != NSLineBreakByWordWrapping) {
+            NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+            paragraphStyle.lineBreakMode = lineBreakMode;
+            attr[NSParagraphStyleAttributeName] = paragraphStyle;
+        }
+        CGRect rect = [self boundingRectWithSize:size
+                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                      attributes:attr context:nil];
+        result = rect.size;
+    } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        result = [self sizeWithFont:font constrainedToSize:size lineBreakMode:lineBreakMode];
+#pragma clang diagnostic pop
+    }
+    return result;
+}
+
+- (CGFloat)widthForFont:(UIFont *)font {
+    return [self sizeForFont:font size:CGSizeMake(HUGE, HUGE) mode:NSLineBreakByWordWrapping].width;
+}
+
+- (CGFloat)heightForFont:(UIFont *)font width:(CGFloat)width {
+    return [self sizeForFont:font size:CGSizeMake(width, HUGE) mode:NSLineBreakByWordWrapping].height;
+}
+
 @end
