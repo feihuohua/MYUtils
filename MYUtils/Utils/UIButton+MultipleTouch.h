6cafb858cfaffa619314c66876660e6c7e1c315f
sunjinshuai <s_mike@163.com>
2017-12-12 10:21:24 +0800

    button防止多次点击

---
+++ MYUtils/Utils/UIButton+MultipleTouch.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+MultipleTouch.h
+//  MYUtils
+//
+//  Created by sunjinshuai on 2017/12/12.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (MultipleTouch)
+
+@property (nonatomic, assign) NSTimeInterval timeInterval;
+
+@end
