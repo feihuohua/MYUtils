efe078a8a0459789b679ad0ec5a5ae076e8fcfef
sunjinshuai <s_mike@163.com>
2018-08-13 20:07:34 +0800

    tableViewCell 倒计时

--- MYUtils/Modules/UIKit/View/MYCountDownViewCell.m
+++ MYUtils/Modules/UIKit/View/MYCountDownViewCell.m
@@ -8,6 +8,7 @@
 
 #import "MYCountDownViewCell.h"
 #import "MYCountDownModel.h"
+#import "MYCountDownManager.h"
 
 @interface MYCountDownViewCell()
 
@@ -20,29 +21,48 @@
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     if (self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier]) {
-        dispatch_queue_t disqueue =  dispatch_queue_create("com.countdown", DISPATCH_QUEUE_CONCURRENT);
-        dispatch_group_t disgroup = dispatch_group_create();
-        dispatch_group_async(disgroup, disqueue, ^{
-            self.link = [CADisplayLink displayLinkWithTarget:self selector:@selector(countDown)];
-            [self.link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
-        });
+        
+        // 监听通知
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(countDownNotification)
+                                                     name:kMYCountDownNotification
+                                                   object:nil];
     }
     return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-}
-
 - (void)setCountDownModel:(MYCountDownModel *)countDownModel {
     _countDownModel = countDownModel;
+    
     self.textLabel.text = countDownModel.title;
+    // 手动刷新数据
+    [self countDownNotification];
 }
 
-- (void)countDown {
-    NSDateFormatter *dateformatter = [[NSDateFormatter alloc] init];
-    dateformatter.dateFormat = @"HH:mm:ss";
-    self.detailTextLabel.text = [NSString stringWithFormat:@"倒计时%@", [dateformatter stringFromDate:[NSDate date]]];
+- (void)countDownNotification {
+    
+    NSInteger timeInterval;
+    if (self.countDownModel.countDownSource) {
+        timeInterval = [[MYCountDownManager manager] timeIntervalWithIdentifier:self.countDownModel.countDownSource];
+    } else {
+        timeInterval = [MYCountDownManager manager].timeInterval;
+    }
+    NSInteger countDown = self.countDownModel.count - timeInterval;
+    /// 当倒计时到了进行回调
+    if (countDown <= 0) {
+        self.detailTextLabel.text = @"活动开始";
+        // 回调给控制器
+        if (self.finishBlock) {
+            self.finishBlock(self.countDownModel);
+        }
+        return;
+    }
+    /// 重新赋值
+    self.detailTextLabel.text = [NSString stringWithFormat:@"倒计时%02zd:%02zd:%02zd", countDown/3600, (countDown/60)%60, countDown%60];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
